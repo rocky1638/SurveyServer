1ac268c6f3b59bd81b197e791ecc931cfec67b4a
Rock Zhou <rockzhou15@gmail.com>
2017-12-23 10:46:27 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000; // environment variable
+app.listen(PORT);
+
+/**
+ * Steps For Heroku Deployment:
+ * Dynamic Port Binding
+ * Specify Node Environment/Version (package.json)
+ * Specify Start Script (package.json)
+ * Create .gitignore file
+ **/
